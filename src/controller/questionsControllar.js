e66d085ffd2b73ecadc0e492954dd62ef93c95e0
dilip-simbiotik <dilip.s@simbiotikgroup.com>
2023-10-20 13:41:51 +0530

    api added for questionbook

--- src/controller/questionsControllar.js
+++ src/controller/questionsControllar.js
@@ -1,82 +1,117 @@
-import questionModel from "../models/questions.js"
+import questionModel from "../models/questions.js";
+import moment from "moment";
 
 const addQuestions = async (req, res) => {
-    const { title, questions, timeline } = req.body;
-    try {
-        const newQuestion = new questionModel({
-            title,
-            questions,
-            timeline
-        });
-        await newQuestion.save();
-        res.status(200).json({ status: true, message: "Question added successfully." });
-    } catch (error) {
-        res.status(500).json({ message: error });
-    }
-}
+  const { title, questions, timeline } = req.body;
+  try {
+    const newQuestion = new questionModel({
+      title,
+      questions,
+      timeline,
+    });
+    await newQuestion.save();
+    res
+      .status(200)
+      .json({ status: true, message: "Question added successfully." });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
 
 const getAllQuestions = async (req, res) => {
-    try {
-        const questionsData = await questionModel.find({}, { __v: 0 });
-        res.status(200).json({ status: true, questionsData });
-    } catch (error) {
-        res.status(500).json({ message: error });
-    }
-}
+  try {
+    const questionsData = await questionModel.find({}, { __v: 0 });
+    res.status(200).json({ status: true, questionsData });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
 
 const updateQuestion = async (req, res) => {
-    try {
-        const questionBookId = req?.params?.id;
-        const { questionId } = req?.query;
-        const { questionTitle, optionType, options } = req.body;
-        const findBookAndUpdate = await questionModel.findOneAndUpdate(
-            {
-                _id: questionBookId,
-                'questions._id': questionId
-            },
-            {
-                $set: {
-                    'questions.$.questionTitle': questionTitle,
-                    'questions.$.optionType': optionType,
-                    'questions.$.options': options,
-                },
-            },
-            { new: true }
-        );
+  try {
+    const questionBookId = req?.params?.id;
+    const { questionId } = req?.query;
+    const { questionTitle, optionType, options } = req.body;
+    const findBookAndUpdate = await questionModel.findOneAndUpdate(
+      {
+        _id: questionBookId,
+        "questions._id": questionId,
+      },
+      {
+        $set: {
+          "questions.$.questionTitle": questionTitle,
+          "questions.$.optionType": optionType,
+          "questions.$.options": options,
+        },
+      },
+      { new: true }
+    );
 
-        if (!findBookAndUpdate) {
-            return res.status(404).json({ status: false, message: 'Question not found!' });
-        }
-
-        res.status(200).json({ status: true, message: 'Question updated successfully.' });
-    } catch (error) {
-        res.status(500).json({ message: error });
+    if (!findBookAndUpdate) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Question not found!" });
     }
-}
+
+    res
+      .status(200)
+      .json({ status: true, message: "Question updated successfully." });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
 
 const deleteQuestion = async (req, res) => {
-    try {
-        const questionBookId = req.params.id;
-        const questionId = req.query.questionId;
+  try {
+    const questionBookId = req.params.id;
+    const questionId = req.query.questionId;
 
-        // Use findOneAndDelete to delete the specific question by its ID
-        const deletedQuestionBook = await questionModel.findOneAndUpdate(
-            { _id: questionBookId },
-            {
-                $pull: { questions: { _id: questionId } }
-            },
-            { new: true }
-        );
+    // Use findOneAndDelete to delete the specific question by its ID
+    const deletedQuestionBook = await questionModel.findOneAndUpdate(
+      { _id: questionBookId },
+      {
+        $pull: { questions: { _id: questionId } },
+      },
+      { new: true }
+    );
 
-        if (!deletedQuestionBook) {
-            return res.status(404).json({ status: false, message: 'Question book not found' });
-        }
-
-        res.status(200).json({ status: true, message: 'Question deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'An error occurred while deleting the question' });
+    if (!deletedQuestionBook) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Question book not found" });
     }
 
-}
+    res
+      .status(200)
+      .json({ status: true, message: "Question deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error occurred while deleting the question" });
+  }
+};
 
-export { addQuestions, getAllQuestions, updateQuestion, deleteQuestion }
+const getQuestionOnTimeline = async (req, res) => {
+  try {
+    const { startDate } = req.query;
+    const start = moment(startDate, "DD/MM/YYYY");
+    const todayDate = moment(new Date(), "DD/MM/YYYY");
+
+    // Calculate the difference in months
+    const monthsDiff = todayDate.diff(start, "months");
+    const findQuestionBook = await questionModel.find({
+      timeline: { $lte: monthsDiff },
+    });
+    res.status(200).json({ status: true, data: findQuestionBook });
+  } catch (error) {
+    res.status(400).json({ status: false, message: 'Something went wrong' });
+  }
+};
+
+export {
+  addQuestions,
+  getAllQuestions,
+  updateQuestion,
+  deleteQuestion,
+  getQuestionOnTimeline,
+};
--- src/routes/questionnaires/questionnaires.js
+++ src/routes/questionnaires/questionnaires.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import authenticateRoles from "../../middleware/auth.js"
-import { addQuestions, deleteQuestion, getAllQuestions, updateQuestion } from '../../controller/questionsControllar.js';
+import { addQuestions, deleteQuestion, getAllQuestions, getQuestionOnTimeline, updateQuestion } from '../../controller/questionsControllar.js';
 let questionnairesRoute = express.Router();
 
-questionnairesRoute.post("/questionform", authenticateRoles(['admin']), addQuestions);
+// Get the questionBook as per the timeline
+questionnairesRoute.get("/getquestions", getQuestionOnTimeline);
+
+// To get all the qusetionsBooks
 questionnairesRoute.get("/questionsList", getAllQuestions);
+
+// Questionaires API' for Admin only
+questionnairesRoute.post("/questionform", authenticateRoles(['admin']), addQuestions);
 questionnairesRoute.put("/updateQuestion/:id", authenticateRoles(['admin']), updateQuestion);
 questionnairesRoute.delete("/deleteQuestion/:id", authenticateRoles(['admin']), deleteQuestion);
 
